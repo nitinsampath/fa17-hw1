0e65aa239f48f0609f68d7e5f86487df0ec5b1d8
nitinsampath <nitinsampath@Nitins-MacBook-Pro.local>
2017-09-19 20:38:35 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,19 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+
+   	a.map!{|elem|
+   		elem = elem.to_i
+   	}
+   	a.map!{|elem|
+   		elem += 2
+   	}
+   	a.delete_if {|n| n%2 == 1}
+
+   	a = a.uniq
+   	a.reject! {|elem| elem>10}
+   	return a.inject(0, :+)
+
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,23 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  temp = a + b
+  return temp ** 2
 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  a = a.sort
+
+  a.map! { |element|
+  	element += 1
+
+  }
+
+  return a
 
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  return first_name + " " + last_name
 
 end
 
